6ab4404316af6bcb4165f231dc7c7b7a2f6f68fd
Tyuiu.KiselevEA@outlook.com <Tyuiu.KiselevEA@outlook.com>
2024-11-04 17:21:38 +0500

    2

--- Tyuiu.KiselevEA.Sprint4.Task0.V4.Test/DataServiceTest.cs
+++ Tyuiu.KiselevEA.Sprint4.Task0.V4.Test/DataServiceTest.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.KiselevEA.Sprint4.Task0.V4.Test
             int[] Array = { 5, 3, 7, 1, 3, 9, 8, 7, 9, 4 };
             int res = ds.GetMultEvenArrEl(Array);
             int wait = 32;
-            Assert.AreEqual(wait,res);
+            CollectionAssert.AreEqual(wait,res);
         }
     }
 }
