0fcab33f34664983481048e4cfae3332ee177a5e
Tyuiu.KiselevEA@outlook.com <Tyuiu.KiselevEA@outlook.com>
2024-11-07 02:17:15 +0500

    12

--- Tyuiu.KiselevEA.Sprint4.Task1.V13.Test/DataServiceTest.cs
+++ Tyuiu.KiselevEA.Sprint4.Task1.V13.Test/DataServiceTest.cs
@@ -1,3 +1,4 @@
+using Tyuiu.KiselevEA.Sprint4.Task1.V13.Lib;
 namespace Tyuiu.KiselevEA.Sprint4.Task1.V13.Test
 {
     [TestClass]
@@ -6,6 +7,11 @@ namespace Tyuiu.KiselevEA.Sprint4.Task1.V13.Test
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new DataService();
+            int[] Array = { 5, 3, 7, 1, 3, 9, 8, 7, 9, 4 };
+            int res = ds.GetMultEvenArrEl(Array);
+            int wait = 32;
+            CollectionAssert.AreEqual(wait, res);
         }
     }
 }
--- Tyuiu.KiselevEA.Sprint4.Task1.V13/Program.cs
+++ Tyuiu.KiselevEA.Sprint4.Task1.V13/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace Tyuiu.KiselevEA.Sprint4.Task1.V13
+﻿using Tyuiu.KiselevEA.Sprint4.Task1.V13.Lib;
+namespace Tyuiu.KiselevEA.Sprint4.Task1.V13
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Киселев Е. А. | СМАРТб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Киселев Егор Алексеевич | СМАРТб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет                *");
+            Console.WriteLine("* произведение ряда по формуле.                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int len;
+            Console.Write("Введите колво элементов");
+            len = Convert.ToInt32(Console.ReadLine());
+
+            int[] numsarry = new int[len];
+            for (int i = 0; i < len; i++)
+
+            {
+                Console.Write("Введите значение " + i + "элемента массива");
+                numsarry[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("массив: ");
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write(numsarry[i] + "/t");
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Итоговый массив:");
+
+
+            int res = ds.Calculate(numsarry);
+            Console.WriteLine(res);
+
+
+
+            Console.ReadKey();
         }
     }
 }
